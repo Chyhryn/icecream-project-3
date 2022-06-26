02341358a03a5456467e0f1aa05f9ff27b310c32
Chyhryn <103328046+Chyhryn@users.noreply.github.com>
2022-06-26 19:44:22 +0000

    Deploying to gh-pages from @ Chyhryn/icecream-project-3@2bb4493f3b9d8c3b2d5dca48ca5320d17028522d 🚀

---
+++ index.2bc4ad53.js
@@ -0,0 +1,2 @@
+document.getElementById("btnFirst").onclick=function(){document.getElementById("content__first").classList.toggle("content-option"),this.classList.toggle("rotate")},document.getElementById("btnSecond").onclick=function(){document.getElementById("content__second").classList.toggle("content-option"),this.classList.toggle("rotate")},document.getElementById("btnThird").onclick=function(){document.getElementById("content__third").classList.toggle("content-option"),this.classList.toggle("rotate")};
+//# sourceMappingURL=index.2bc4ad53.js.map
